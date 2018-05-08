d9fa5550e95fbae94126732414f75fd0e659ae1a
Jordan Featherstone <jordanfeatherstone@gmail.com>
2018-05-08 16:17:11 +0100

    Creating forms the right way!

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -5,5 +5,6 @@ from boards import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
-    re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics')
+    re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 ]
